cb58b5a70e736be296d16b49357a592087c6e6eb
Ahmad Abbas <ahmad110abbas@gmail.com>
2020-12-07 00:06:38 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,40 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<style type="text/css">
+  #ftbg{
+  background-color: #fabd06;
+  position: relative;
+}
+#logo{
+  font-size: 25px;
+  margin-top: 15px;
+}
+#clr{
+  color: red;
+}
+</style>
+<section id="ftbg">
+
+  <footer class="page-footer font-small blue">
+
+    <div class="row py-0">
+      <div class="col-sm">
+        <div class="footer-copyright text-center py-2" style="padding: :0;margin:0;">
+         <i class="fa fa-twitter" style="font-size:40px;"></i>
+         <i class="fa fa-whatsapp" style="font-size:40px;"></i>
+         <i class="fa fa-youtube" style="font-size:40px;"></i>
+         <i class="fa fa-skype" style="font-size:40px;"></i>
+       </div>
+     </div>
+     <div class="col-sm text-center">
+       <p id="logo">Ced<span id="clr">Cab</span></p>
+     </div>
+     <div class="col-sm">
+      <div class="footer-copyright text-center py-3" style="font-size:20px;">© 2020 Copyright:
+        <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
+      </div>
+    </div>
+  </div>
+
+</footer>
+
+</section>
